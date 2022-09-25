93b3a000a00bf7036bc4bb71d7e5da12f46353de
Shreyash2571 <patel494@uwindsor.ca>
2022-09-25 17:28:25 -0400

    Adding get user functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,23 @@
 const User = require("../mongoose/model/user.model")
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const { ROUNDS } = require('../mongoose/mongoose')
+
+const getUserById = async (req, res) => {
+
+    try {
+        const { id } = req.params
+        if (!mongoose.Types.ObjectId.isValid(id))
+            return res.status(404).json({ message: 'User not found' })
+        const user = await User.findById(id)
+        if (user === null) return res.status(404).send({ message: 'User not found' })
+        return res.status(200).send({ data: { email: user.email, firstName: user.firstName, lastName: user.lastName, image: user.image } })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send({ message: 'Internal Server Error' })
+    }
+
+}
 
 const editUser = async (req, res) => {
     try {
@@ -12,4 +31,4 @@ const editUser = async (req, res) => {
     }
 }
 
-module.exports = { editUser }
+module.exports = { getUserById, editUser }
